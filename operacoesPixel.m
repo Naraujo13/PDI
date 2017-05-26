7eb9d40dc352b099ffbe400670d943f0c25dccc8
Nícolas Oreques de Araujo <nodaraujo@inf.ufpel.edu.br>
2017-05-26 14:35:41 -0300

    Add files via upload

---
+++ operacoesPixel.m
@@ -0,0 +1,20 @@
+pkg load image;
+
+A = imread('toycars1.png');
+B = imread('toycars2.png');
+
+A2 = im2bw(A);
+B2 = im2bw(B);
+
+subplot(2,3,1), imshow(A2);
+subplot(2,3,2), imshow(B2);
+
+Output = xor (A2, B2);
+
+subplot(2,3,3), imshow(Output);
+
+I = imread('rice.png');
+
+I2 = im2bw(I, 0.5);
+subplot(2, 3, 4), imshow(I);
+subplot(2, 3, 5), imshow(I2);
